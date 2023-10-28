5bc666796d306ab90bfd7cd97911db94d34cf969
Muskan Singh <muskan.s.del@gmail.com>
2023-10-28 17:59:27 +0530

    dsa

--- lectures/18-linkedlist/code/src/com/kunal/Main.java
+++ lectures/18-linkedlist/code/src/com/kunal/Main.java
@@ -3,6 +3,7 @@ package com.kunal;
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
+
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
